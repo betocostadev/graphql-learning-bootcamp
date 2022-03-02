e197748505aa6c4ebeba9989ee5938531f997672
betocostadev <robertomarx@gmail.com>
2022-03-02 19:31:02 -0300

    added some array endpoints to query

--- graphql-basics/src/fakeDB.js
+++ graphql-basics/src/fakeDB.js
@@ -1,14 +1,15 @@
 // Since we are not using a DB here, there is the demo data below
 // Demo user data
-const users = [
+const dbUsers = [
   { id: '1', name: 'Beto', email: 'beto@example.com', age: 36 },
   { id: '2', name: 'Andrew', email: 'ajdrew@example.com', age: 31 },
   { id: '3', name: 'Ox', email: 'oxitona@example.com', age: 36 },
   { id: '4', name: 'Carolina', email: 'carol@pucc.com', age: 30 },
   { id: '5', name: 'Antonio', email: 'tonho@example.com', age: 40 },
+  { id: '6', name: 'Joaquim Floriano', email: 'joaflora@example.com' },
 ]
 
-const posts = [
+const dbPosts = [
   { id: '43', title: 'Testing graphql', body: 'This is some text', published: true, author: '1' },
   {
     id: '65',
@@ -35,7 +36,7 @@ const posts = [
   },
 ]
 
-const comments = [
+const dbComments = [
   { id: 432, author: '2', post: '43', text: 'Great article' },
   { id: 321, author: '1', post: '65', text: 'Great I liked it a lot' },
   { id: 329, author: '4', post: '33', text: 'Hard for me' },
@@ -45,4 +46,4 @@ const comments = [
   { id: 872, author: '1', post: '35', text: 'Nice to see an article about React Native' },
 ]
 
-export { users, posts, comments }
+export { dbUsers, dbPosts, dbComments }
--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,35 +1,60 @@
 import { createServer } from '@graphql-yoga/node'
+import { dbUsers, dbPosts, dbComments } from './fakeDB'
 
 // GraphQL Scalar types: String, Boolean, Numbers (Int, Float), ID
-// Non-Scalar types: Arrays, objects
+// Non-Scalar types: Arrays, Objects
 
 // Type Definitions (Schema)
 const typeDefs = `
   type Query {
+    users(query: String): [User!]!
+    me: User!
+    post: Post!
+  }
+
+  type User {
     id: ID!
     name: String!
-    age: Int!
-    employed: Boolean!
-    gpa: Float
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `
 // Resolvers
 const resolvers = {
   Query: {
-    id() {
-      return 'aksoe-93203'
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return dbUsers
+      }
+
+      return dbUsers.filter((user) => {
+        return (
+          user.name.toLowerCase().includes(args.query.toLowerCase()) ||
+          user.email.toLowerCase().includes(args.query.toLowerCase())
+        )
+      })
     },
-    name() {
-      return 'Beto Costa'
+    me() {
+      return {
+        id: '0010',
+        name: 'Beto',
+        email: 'beto@example.com',
+      }
     },
-    age() {
-      return 35
-    },
-    employed() {
-      return true
-    },
-    gpa() {
-      return null
+    post() {
+      return {
+        id: '02110',
+        title: 'The post title',
+        body: 'This is the post body, can you see it?',
+        published: true,
+      }
     },
   },
 }
